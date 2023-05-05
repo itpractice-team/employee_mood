934480a157991a8f095a5f8f968469fb130918aa
Aleksandr Usolcev <alexusolev@gmail.com>
2023-05-05 16:29:08 +0300

    user avatar field

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -87,6 +87,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'users.User'
--- backend/users/models.py
+++ backend/users/models.py
@@ -12,34 +12,30 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
         unique=True,
         max_length=255,
-        blank=False,
+        blank=False
     )
     first_name = models.CharField(
         _('first name'),
         max_length=30,
-        blank=True,
+        blank=True
     )
     last_name = models.CharField(
         _('last name'),
         max_length=150,
+        blank=True
+    )
+    avatar = models.ImageField(
+        upload_to='users/avatars/',
         blank=True,
+        null=True
     )
     is_staff = models.BooleanField(
         _('staff status'),
-        default=False,
-        help_text=_(
-            'Designates whether the user can log into '
-            'this admin site.'
-        ),
+        default=False
     )
     is_active = models.BooleanField(
         _('active'),
-        default=True,
-        help_text=_(
-            'Designates whether this user should be '
-            'treated as active. Unselect this instead '
-            'of deleting accounts.'
-        ),
+        default=True
     )
     date_joined = models.DateTimeField(
         _('date joined'),
@@ -49,3 +45,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
