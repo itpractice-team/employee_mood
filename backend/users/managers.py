966f9f6f3eafa369e0290cc7acdc154168da1556
Aleksandr Usolcev <alexusolev@gmail.com>
2023-05-05 16:09:31 +0300

    change user model

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -1,13 +1,13 @@
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
+
     use_in_migrations = True
 
-    def create_user(self, email, password=None, **extra_fields):
-
+    def _create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('Email обязателен')
+            raise ValueError('The given email must be set')
 
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
@@ -15,9 +15,22 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
 
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(email, password, **extra_fields)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must have is_staff=True.'
+            )
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must have is_superuser=True.'
+            )
+
+        return self._create_user(email, password, **extra_fields)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,22 +1,51 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 from .managers import UserManager
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
+
     email = models.EmailField(
-        verbose_name='Электронная почта',
-        unique=True
+        unique=True,
+        max_length=255,
+        blank=False,
     )
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    first_name = models.CharField(
+        _('first name'),
+        max_length=30,
+        blank=True,
+    )
+    last_name = models.CharField(
+        _('last name'),
+        max_length=150,
+        blank=True,
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into '
+            'this admin site.'
+        ),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be '
+            'treated as active. Unselect this instead '
+            'of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(
+        _('date joined'),
+        default=timezone.now,
+    )
 
     objects = UserManager()
 
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-
-    def __str__(self):
-        return self.email
+    USERNAME_FIELD = 'email'
