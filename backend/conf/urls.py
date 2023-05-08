e8a264294533c48e246ced091033b94ccfd0ce61
Aleksandr Usolcev <alexusolev@gmail.com>
2023-05-08 17:18:11 +0300

    users app admin panel

--- backend/conf/urls.py
+++ backend/conf/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = 'Employee Mood'
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('api.urls'))
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,17 +1,48 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.db.models import Count
 
 from .models import Department, Position, User
 
 
-class DepartmentAdmin(admin.ModelAdmin):
-    pass
+class EmployeesCountMixin:
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(employees_count=Count('employees'))
+        return queryset
+
+    def employees_count(self, obj):
+        return obj.employees_count
+
+    employees_count.short_description = 'Сотрудников'
 
 
-class PositionAdmin(admin.ModelAdmin):
-    pass
+class UserInline(admin.TabularInline):
+    model = User
+    fields = ('last_name', 'first_name', 'email', 'phone')
+    readonly_fields = ('last_name', 'first_name', 'email', 'phone')
+    show_change_link = True
+    show_full_result_count = True
+    can_delete = False
+    extra = 0
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
+@admin.register(Department)
+class DepartmentAdmin(EmployeesCountMixin, admin.ModelAdmin):
+    list_display = ('name', 'employees_count')
+    inlines = [UserInline,]
+
+
+@admin.register(Position)
+class PositionAdmin(EmployeesCountMixin, admin.ModelAdmin):
+    list_display = ('name', 'employees_count')
+    inlines = [UserInline,]
+
+
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'first_name', 'last_name', 'password')}),
@@ -31,11 +62,11 @@ class CustomUserAdmin(UserAdmin):
                        ),
         }),
     )
-    list_display = ('email', 'first_name', 'last_name', 'role', 'department')
+    list_display = ('email', 'first_name', 'last_name',
+                    'role', 'department', 'position')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
 
-
-admin.site.register(Department, DepartmentAdmin)
-admin.site.register(Position, PositionAdmin)
-admin.site.register(User, CustomUserAdmin)
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('department', 'position')
